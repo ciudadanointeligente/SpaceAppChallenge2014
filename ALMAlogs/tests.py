929c7dff51cfdb6a32d0246aa5b5c641cd72a5be
Marcel Augsburger <maugsburger@gmail.com>
2014-04-12 13:50:36 -0300

    Added basic testing

--- ALMAlogs/tests.py
+++ ALMAlogs/tests.py
@@ -1,3 +1,13 @@
+# coding=utf-8
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+class IndexTextCase(TestCase):
+    def setUp(self):
+        pass
+    def test_get_home(self):
+        '''gets home page'''
+        url = reverse("home")
+        self.assertTrue(url)
