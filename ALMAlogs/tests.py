2405784c45b467703036deb8db95429931ebaae4
Marcel Augsburger <maugsburger@gmail.com>
2014-04-12 15:58:41 -0300

    add test line

--- ALMAlogs/tests.py
+++ ALMAlogs/tests.py
@@ -2,8 +2,10 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.test.client import Client
+from .models import Line
 
 # Create your tests here.
+line_raw = '<Debug TimeStamp="2014-04-03T01:46:02.972" File="alma.Control.ObservingModes.LocalOscilatorThread"  Line="98" Routine="run" Host="gas01" Process="CONTROL/ACC/javaContainer" SourceObject="CONTROL/Array014" Thread="Thread-26724581" LogId="95269" Audience="Developer"><![CDATA[Waiting 0.964 seconds for subscan 19 to start.]]></Debug>'
 
 class IndexTextCase(TestCase):
     def setUp(self):
@@ -15,3 +17,22 @@ class IndexTextCase(TestCase):
         client = Client()
         response = client.get(url)
         self.assertEquals(response.status_code, 200)
+
+class LogParserCase(TestCase):
+    def atest_parser_line(self):
+        '''get one line, one love'''
+        parser = LogsParser()
+        line = parser.parse_line(line_raw)
+        self.assertEquals(line, 'Waiting 0.964 seconds for subscan 19 to start.') 
+
+
+class LineCase(TestCase):
+    def test_lineclass(self):
+        '''instance a line'''
+        line = Line()
+        self.assertTrue(line)
+
+    def test_line_attr(self):
+        '''get attribute'''
+        line = Line.objects.create(raw=line_raw)
+        self.assertEquals(line.raw, line_raw)
